386aee4a00f8b5582ce588ed23e1669a61e56de7
huangzehai <chaogeek@foxmail.com>
2020-05-17 23:50:15 +0800

    add example of expression function parameter.

--- src/main/scala/example/FuncParamExample.scala
+++ src/main/scala/example/FuncParamExample.scala
@@ -3,8 +3,14 @@ package example
 object FuncParamExample {
   def main(args: Array[String]): Unit = {
     val numbers = 1 to 10
-    val list = numbers.map(add _).toList
+    val list = numbers.map(add).toList
     list foreach println
+
+    //Expression as function parameter.
+    val result = add {
+      3 + 6
+    }
+    println(s"result is $result")
   }
 
 
